bcc9552824c76708119d4b61bd6eeac26dc5b2df
arjun <arjundashrath@gmail.com>
2022-06-02 16:21:51 +0530

    cmtmsg

--- dist/index.js
+++ dist/index.js
@@ -2744,12 +2744,16 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
         var repo = process.env["GITHUB_REPOSITORY"].split("/")[1];
         var owner = process.env["GITHUB_REPOSITORY"].split("/")[0];
         var runId = process.env["GITHUB_RUN_ID"];
+        var secretsString = "";
+        _actions_core__WEBPACK_IMPORTED_MODULE_1__.getMultilineInput("secrets").forEach((secret) => { secretsString = secretsString + secret + ","; });
+        secretsString = secretsString.slice(0, -1);
+        console.log(secretsString);
         var url = "https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId + "&secrets=secret1,secret2";
         try {
             // TODO: Replace owner, repo, runId, and list of secrets in the below API request with actual values
-            var response = yield _http.get(
-            //url
-            `https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`);
+            var response = yield _http.get(url
+            //`https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`
+            );
             // The response should be something like
             // {"repo":"step-security/secureworkflows","runId":"123","areSecretsSet":true,"secrets":[{"Name":"secret1","Value":"val1"},{"Name":"secret2","Value":"valueofsecret2"}]}
             // console.log(url)
@@ -2766,14 +2770,17 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
                     break;
                 }
                 else {
-                    yield sleep(10000);
-                    console.log("https://app.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId);
+                    yield sleep(9000);
+                    console.log("Visit the URL to input the secrets:");
+                    console.log("https://int1.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId);
                 }
+                //Go to URL to enter secrets
                 // If areSecretsSet is set to false, it means the secrets are not yet set by user, so wait for 10 seconds and print the link.
                 // Link should be of the form: https://app.stepsecurity.io/secrets?owner=step-security&repo=secureworkflows&runId=123
                 // If areSecretsSet is set to true, take the secrets and set to output using core.setOutput(secretname, secretvalue)
                 // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
                 console.log(`retrying...`);
+                let xyz = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("secrets");
                 counter++;
                 if (counter > 60) {
                     console.log("timed out");
--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,21 @@ import * as core from "@actions/core"
     var owner = process.env["GITHUB_REPOSITORY"].split("/")[0];
     var runId = process.env["GITHUB_RUN_ID"];
 
+    var secretsString = ""
+
+    core.getMultilineInput("secrets").forEach( (secret) => {secretsString = secretsString + secret + ",";})
+
+    secretsString = secretsString.slice(0, -1);
+
+    console.log(secretsString)
+
     var url = "https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=" + owner + "&repo=" + repo + "&runId="+ runId+ "&secrets=secret1,secret2";
 
     try {
       // TODO: Replace owner, repo, runId, and list of secrets in the below API request with actual values
       var response = await _http.get(
-        //url
-        `https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`
+        url
+        //`https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`
       );
       // The response should be something like
       // {"repo":"step-security/secureworkflows","runId":"123","areSecretsSet":true,"secrets":[{"Name":"secret1","Value":"val1"},{"Name":"secret2","Value":"valueofsecret2"}]}
@@ -38,17 +46,22 @@ import * as core from "@actions/core"
         break;
         }
         else{
-          await sleep(10000);
+          await sleep(9000);
 
-          console.log("https://app.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId)
+          console.log("Visit the URL to input the secrets:")
+
+          console.log("https://int1.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId)
 
         }
+
+        //Go to URL to enter secrets
 
         // If areSecretsSet is set to false, it means the secrets are not yet set by user, so wait for 10 seconds and print the link.
         // Link should be of the form: https://app.stepsecurity.io/secrets?owner=step-security&repo=secureworkflows&runId=123
         // If areSecretsSet is set to true, take the secrets and set to output using core.setOutput(secretname, secretvalue)
         // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
         console.log(`retrying...`);
+        let xyz = core.getInput("secrets")
         counter++;
         if (counter > 60) {
           console.log("timed out");
