751d21f3a29ea23476646edf1e42b7ca3d7ed361
arjundashrath <54043589+arjundashrath@users.noreply.github.com>
2022-08-04 21:13:27 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,12 @@ import * as core from "@actions/core"
   let _http = new httpm.HttpClient();
   _http.requestOptions = { socketTimeout: 3 * 1000 };
   var counter = 0;
+  var sendToSlack = true;
   while (true) {
 
     var repo = process.env["GITHUB_REPOSITORY"].split("/")[1];
     var owner = process.env["GITHUB_REPOSITORY"].split("/")[0];
     var runId = process.env["GITHUB_RUN_ID"];
-    var authIDToken = core.getIDToken()
-
-    var headers = new Headers().append("Authorization", "Bearer " + authIDToken)
 
     var secretsString = ""
 
@@ -25,7 +23,7 @@ import * as core from "@actions/core"
 
     try {
       var response = await _http.get(
-        url, {headers: {Authorization: 'Bearer ' + authIDToken}}
+        url
         //`https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`
       );
       // The response should be something like
@@ -47,9 +45,30 @@ import * as core from "@actions/core"
         else{
           await sleep(9000);
 
-          console.log("Visit the URL to input the secrets:")
+          if(process.env.SLACK_WEBHOOK_URL){
+            
+            if(sendToSlack){
+              var slackPostData = {"text":"https://int1.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId}
+              
+              var slackresponse = await _http.postJson(
+                process.env.SLACK_WEBHOOK_URL, slackPostData
+                //`https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`
+              );
+              // The response should be something like
+              // {"repo":"step-security/secureworkflows","runId":"123","areSecretsSet":true,"secrets":[{"Name":"secret1","Value":"val1"},{"Name":"secret2","Value":"valueofsecret2"}]}
+              
+              if (slackresponse.statusCode === 200) {
+                console.log("Visit the URL sent to Slack to input the secrets.")
+                sendToSlack = false;
+              }else{
+                console.log("Error sending to Slack. Status code: " + slackresponse.statusCode)
+              }}
+          }else{
+            console.log("Visit the URL to input the secrets:")
+          
+            console.log("https://int1.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId)
 
-          console.log("https://int1.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId)
+          }
 
         }
         console.log(`retrying...`);
