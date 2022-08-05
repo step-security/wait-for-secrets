439c1a6147119070f463362d539eb7a8cb97c6b1
Varun Sharma <varunsh@stepsecurity.io>
2022-08-04 23:18:30 -0700

    Update client to use auth header

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as httpm from "@actions/http-client";
-import * as core from "@actions/core"
+import * as core from "@actions/core";
 
 (async () => {
   // call API
@@ -7,25 +7,27 @@ import * as core from "@actions/core"
   _http.requestOptions = { socketTimeout: 3 * 1000 };
   var counter = 0;
   while (true) {
-
     var repo = process.env["GITHUB_REPOSITORY"].split("/")[1];
     var owner = process.env["GITHUB_REPOSITORY"].split("/")[0];
     var runId = process.env["GITHUB_RUN_ID"];
-    var authIDToken = core.getIDToken()
+    var authIDToken = await core.getIDToken();
 
-    var headers = new Headers().append("Authorization", "Bearer " + authIDToken)
+    var secretsString = "";
 
-    var secretsString = ""
-
-    core.getMultilineInput("secrets").forEach( (secret) => {secretsString = secretsString + secret + ",";})
+    core.getMultilineInput("secrets").forEach((secret) => {
+      secretsString = secretsString + secret + ",";
+    });
 
     secretsString = secretsString.slice(0, -1);
 
-    var url = "https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=" + owner + "&repo=" + repo + "&runId="+ runId+ "&secrets=" + secretsString;
+    var url =
+      "https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?secrets=" +
+      secretsString;
 
     try {
       var response = await _http.get(
-        url, {headers: {Authorization: 'Bearer ' + authIDToken}}
+        url,
+        { headers: { Authorization: "Bearer " + authIDToken } }
         //`https://9046hrh9g0.execute-api.us-west-2.amazonaws.com/v1/secrets?owner=step-security&repo=secureworkflows&runId=123&secrets=secret1,secret2`
       );
       // The response should be something like
@@ -33,27 +35,32 @@ import * as core from "@actions/core"
       if (response.message.statusCode === 200) {
         const body: string = await response.readBody();
         const respJSON = JSON.parse(body);
-        
+
         if (respJSON.areSecretsSet === true) {
           //something
-          respJSON.secrets.forEach( (secret) => {
+          respJSON.secrets.forEach((secret) => {
             core.setOutput(secret.Name, secret.Value);
             core.setSecret(secret.Value);
-        });
+          });
 
-        console.log("Successfully set secrets!")
-        break;
-        }
-        else{
+          console.log("Successfully set secrets!");
+          break;
+        } else {
           await sleep(9000);
 
-          console.log("Visit the URL to input the secrets:")
+          console.log("Visit the URL to input the secrets:");
 
-          console.log("https://int1.stepsecurity.io/secrets?owner=" + owner + "&repo=" + repo + "&runId=" + runId)
-
+          console.log(
+            "https://int1.stepsecurity.io/secrets?owner=" +
+              owner +
+              "&repo=" +
+              repo +
+              "&runId=" +
+              runId
+          );
         }
         console.log(`retrying...`);
-        let xyz = core.getInput("secrets")
+
         counter++;
         if (counter > 60) {
           console.log("timed out");
