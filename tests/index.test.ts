3da331f5a884b237f087159f0c4056071f68c457
arjun <arjundashrath@gmail.com>
2022-12-15 20:41:02 +0530

    fix bug

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,17 +7,22 @@ import {
 
 import * as cp from "child_process";
 
-process.env["GITHUB_REPOSITORY"] = "step-security/test";
-process.env["GITHUB_RUN_ID"] = "12345";
-process.env["ACTIONS_ID_TOKEN_REQUEST_URL"] = "test_url";
-process.env["ACTIONS_ID_TOKEN_REQUEST_TOKEN"] = "test_token";
+
 
 
 test('generateSecretURL()', () => {
+  process.env["GITHUB_REPOSITORY"] = "step-security/test";
+  process.env["GITHUB_RUN_ID"] = "12345";
+  process.env["ACTIONS_ID_TOKEN_REQUEST_URL"] = "test_url";
+  process.env["ACTIONS_ID_TOKEN_REQUEST_TOKEN"] = "test_token";
   expect(generateSecretURL()).toBe("https://app.stepsecurity.io/secrets/step-security/test/12345");
 });
 
 test('setSecrets()', () => {
+  process.env["GITHUB_REPOSITORY"] = "step-security/test";
+  process.env["GITHUB_RUN_ID"] = "12345";
+  process.env["ACTIONS_ID_TOKEN_REQUEST_URL"] = "test_url";
+  process.env["ACTIONS_ID_TOKEN_REQUEST_TOKEN"] = "test_token";
   var secrets = ["Mock_Secret_1", "Mock_Secret_2"]
   expect(setSecrets(secrets)).toBe(undefined);
 });
