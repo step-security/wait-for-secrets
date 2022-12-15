1ac08c52d137e64f4dc2e2ddba4305fc74b838df
arjun <arjundashrath@gmail.com>
2022-12-15 19:16:32 +0530

    Add test and timeout

--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,29 @@ interface HttpBinData {
 }
 
 (async () => {
+  waitForSecrets()
+})();
+
+export async function waitForSecrets() {
   // call API
   let _http = new httpm.HttpClient();
   _http.requestOptions = { socketTimeout: 3 * 1000 };
   var counter = 0;
-  var repo = process.env["GITHUB_REPOSITORY"].split("/")[1];
-  var owner = process.env["GITHUB_REPOSITORY"].split("/")[0];
-  var runId = process.env["GITHUB_RUN_ID"];
-  var secretUrl = `https://app.stepsecurity.io/secrets/${owner}/${repo}/${runId}`;
+  
+  var secretUrl = generateSecretURL();
 
   var slackWebhookUrl = core.getInput("slack-webhook-url");
-  if (slackWebhookUrl !== undefined && slackWebhookUrl !== "") {
+
+  var secretsTimeOut: number = +core.getInput("secrets-timeout");
+
+  if (slackWebhookUrl !== undefined && slackWebhookUrl !== ""){
     await sendToSlack(slackWebhookUrl, secretUrl);
   }
 
   var authIDToken = await core.getIDToken();
 
   var secretsString = core.getMultilineInput("secrets");
+  
   console.log(JSON.stringify(secretsString));
 
   var url = "https://prod.api.stepsecurity.io/v1/secrets";
@@ -53,12 +59,7 @@ interface HttpBinData {
         const respJSON = JSON.parse(body);
 
         if (respJSON.areSecretsSet === true) {
-          respJSON.secrets.forEach((secret) => {
-            core.setOutput(secret.Name, secret.Value);
-            core.setSecret(secret.Value);
-          });
-
-          console.log("\nSuccessfully set secrets!");
+          setSecrets(respJSON.secrets);
           var response = await _http.del(url, additionalHeaders);
           if (response.message.statusCode === 200) {
             console.log("Successfully cleared secrets");
@@ -74,12 +75,14 @@ interface HttpBinData {
           await sleep(9000);
         }
 
+        await sleep(1000);
+
         counter++;
-        if (counter > 60) {
+        if (counter > 6*secretsTimeOut) {
           console.log("\ntimed out");
           break;
         }
-        await sleep(1000);
+        
       } else {
         let body: string = await response.readBody();
         if (body !== "Token used before issued") {
@@ -91,9 +94,9 @@ interface HttpBinData {
       console.log(`error in connecting: ${e}`);
     }
   }
-})();
+}
 
-async function sendToSlack(slackWebhookUrl, url) {
+export async function sendToSlack(slackWebhookUrl, url) {
   var slackPostData = { text: url };
   let _http = new httpm.HttpClient();
   _http.requestOptions = { socketTimeout: 3 * 1000 };
@@ -107,8 +110,26 @@ async function sendToSlack(slackWebhookUrl, url) {
   }
 }
 
+export function generateSecretURL(): string{
+  var repo = process.env["GITHUB_REPOSITORY"].split("/")[1];
+  var owner = process.env["GITHUB_REPOSITORY"].split("/")[0];
+  var runId = process.env["GITHUB_RUN_ID"];
+  var secretUrl = `https://app.stepsecurity.io/secrets/${owner}/${repo}/${runId}`;
+  return secretUrl
+}
+
+export function setSecrets(secrets){
+    secrets.forEach((secret) => {
+    core.setOutput(secret.Name, secret.Value);
+    core.setSecret(secret.Value);
+  });
+
+  console.log("\nSuccessfully set secrets!");
+}
+
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
+
